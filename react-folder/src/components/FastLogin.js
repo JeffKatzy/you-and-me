06bc4dc430dc897ed3ec0cd445de1ef00f7f79df
JeffKatzy <JeffreyEricKatz@gmail.com>
2016-12-13 15:27:26 -0500

    Update FastLogin.js

--- react-folder/src/components/FastLogin.js
+++ react-folder/src/components/FastLogin.js
@@ -7,6 +7,9 @@ import boardRequest from '../actions/boardRequest'
 
 class FastLogin extends Component {
   handleOnClick(event){
+//     This email and password would be better stored in the secrets.yml of your rails app.
+//     So logUserIn would make the ajax call to ultimately log in the user.
+    
     this.props.logUserIn({email: "drew@d.com", password:"me"})
   }
 
